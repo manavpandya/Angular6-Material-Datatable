7fa08419ec5a42af6fc4c6170e042b36a1149eec
manav <manav@upforcetech.com>
2018-05-04 17:55:42 +0530

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { MyTableComponent } from "./my-table/my-table.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule
+} from "@angular/material";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MyTableComponent],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
